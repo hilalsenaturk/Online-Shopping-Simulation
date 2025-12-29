b2989e3b62f1b784bca0f5a7354b3eb5cb294ff3
hilalsenaturk <hilalsenaturk2@gmail.com>
2025-12-29 16:03:01 +0300

    storage.py

--- storage.py
+++ storage.py
@@ -3,16 +3,19 @@ import os
 import shutil
 from datetime import datetime
 
+products_path = os.path.join("data", "products.json")
+receipts_path = os.path.join("data", "receipts.json")
+
 def ensure_storage_structure(base_dir="data"):
     if not os.path.exists(base_dir):
         os.makedirs(base_dir)
         print(f"Directory {base_dir} created")
 
-def load_json(path):
-    if not os.path.exists(path):
+def load_json(products_path):
+    if not os.path.exists(products_path):
         return []
     try:
-        with open(path, 'r') as f:
+        with open(products_path, 'r') as f:
             return json.load(f)
     except json.decoder.JSONDecodeError:
         return []
@@ -21,6 +24,20 @@ def load_json(path):
     except Exception as e:
         print(f"Error loading json file {e}")
         return []
+
+def save_receipt(receipt):
+    existing_data = []
+    if os.path.exists(receipts_path):
+        with open(receipts_path, "r", encoding="utf-8") as file:
+            try:
+                existing_data = json.load(file)
+            except:
+                existing_data = []
+    existing_data.append(receipt)
+    with open(receipts_path, "w", encoding="utf-8") as file:
+        json.dump(existing_data, file, indent=4, ensure_ascii=False)
+
+
 def write_json(path, data):
     try:
         with open(path, 'w', encoding='utf-8') as f:
@@ -28,7 +45,6 @@ def write_json(path, data):
     except Exception as e:
         print(f"Error writing json file {e}")
         return []
-
 
 
 
